cf1a07b03fa6db906f7e0a226f8ecce37d18e612
gxlpes <guilhermxlopes@gmail.com>
2024-02-05 14:55:57 -0300

    import as es modules js

--- retry.js
+++ retry.js
@@ -1,60 +1,3 @@
-const { proto } = require("@whiskeysockets/baileys");
-
-export const makeRetryHandler = () => {
-  const messagesMap = {};
-
-  const addMessage = async (message) => {
-    const id = message.key.id ?? "";
-
-    messagesMap[id] = {
-      message: cleanMessage(message),
-      ts: Date.now(),
-    };
-
-    return message;
-  };
-
-  const getMessage = (msgKey) => {
-    return messagesMap[msgKey].message;
-  };
-
-  const removeMessage = (msgKey) => {
-    delete messagesMap[msgKey];
-  };
-
-  const getMessageKeys = () => {
-    return Object.keys(messagesMap);
-  };
-
-  const cleanMessage = (message) => {
-    const msg = message.message ?? {};
-    return msg;
-  };
-
-  const clearObseleteMessages = () => {
-    const keys = Object.keys(messagesMap);
-    keys.forEach((key) => {
-      const ts = messagesMap[key].ts;
-      if (Date.now() - ts > 60_000) {
-        removeMessage(key);
-      }
-    });
-  };
-
-  return {
-    addMessage,
-    getMessage,
-    removeMessage,
-    getMessageKeys,
-    cleanMessage,
-    clearObseleteMessages,
-    getHandler: (message) => {
-      const msg = getMessage(message.id ?? "");
-      console.log("Retrying message", msg);
-      return msg;
-    },
-  };
-};
 const makeRetryHandler = () => {
   const messagesMap = {};
 
@@ -110,3 +53,5 @@ const makeRetryHandler = () => {
     },
   };
 };
+
+export { makeRetryHandler };
